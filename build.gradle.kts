4551fef07c5484cd74beda99099bd4abc3f3f008
deturpant <vlad3032.kozlov@yandex.ru>
2024-03-25 21:37:46 +0300

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.2.2"
 }
 
 group = "ru.deturpant"
@@ -14,6 +16,26 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+//Spring
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.3")
+}
+//Lombok
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+//Postgresql
+dependencies {
+    implementation("org.postgresql:postgresql:42.7.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+}
 tasks.test {
     useJUnitPlatform()
 }
